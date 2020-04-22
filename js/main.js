7f977ec7227b9eac295e1c83a82ca7a38399bfb6
Dennis Myasnyankin <dennis415@live.com>
2020-04-22 11:05:46 -0700

    Add d3 tip library and add tooltip to vis

--- js/main.js
+++ js/main.js
@@ -20,6 +20,15 @@ var g = d3
 	.append("g")
 	.attr("transform", "translate(" + margins.left + "," + margins.top + ")");
 
+// Add Tooltip
+var tip = d3
+	.tip()
+	.attr("class", "d3-tip")
+	.html(function(d) {
+		return d;
+	});
+g.call(tip);
+
 // Y Scale
 var y = d3
 	.scaleLinear()
@@ -34,23 +43,25 @@ var x = d3
 	.range([0, width]);
 
 // Radius Scale (population)
-var pop_area = d3.scaleLinear()
+var pop_area = d3
+	.scaleLinear()
 	.domain([2000, 1500000000])
 	// .domain([2000, 1400000000])
-	.range([25*Math.PI, 1200*Math.PI])
+	.range([25 * Math.PI, 1200 * Math.PI]);
 
-	var yellow = d3.interpolateYlGn(0), // "rgb(255, 255, 229)"
+var yellow = d3.interpolateYlGn(0), // "rgb(255, 255, 229)"
 	yellowGreen = d3.interpolateYlGn(0.5), // "rgb(120, 197, 120)"
 	green = d3.interpolateYlGn(1); // "rgb(0, 69, 41)"
 
 // Ordinal Scale for Country colors
-var color = d3.scaleOrdinal(d3.schemePaired)
+var color = d3.scaleOrdinal(d3.schemePaired);
 // var color = d3.scaleOrdinal(d3.schemeBlues[9])
 // var color = d3.scaleOrdinal(d3.schemeDark2)
 // var color = d3.scaleOrdinal(d3.schemeSet1)
 
 // Y LABEL
-var yLabel = g.append("text")
+var yLabel = g
+	.append("text")
 	.attr("y", -60)
 	.attr("x", -(height / 2))
 	.attr("font-size", "20px")
@@ -59,15 +70,17 @@ var yLabel = g.append("text")
 	.text("Life Expectancy (Years)");
 
 // X LABEL
-var xLabel = g.append("text")
+var xLabel = g
+	.append("text")
 	.attr("y", height + 60)
 	.attr("x", width / 2)
 	.attr("font-size", "20px")
 	.attr("text-anchor", "middle")
 	.text("GDP Per Capita ($)");
 
-var yearLabel = g.append("text")
-	.attr("y", height -10)
+var yearLabel = g
+	.append("text")
+	.attr("y", height - 10)
 	.attr("x", width - 40)
 	.attr("font-size", "30px")
 	.attr("opacity", "0.3")
@@ -91,29 +104,33 @@ var yAxisCall = d3.axisLeft(y);
 yAxisGroup.call(yAxisCall);
 
 // Add Legend
-var continents = ['europe', 'asia', 'americas', 'africa']
+var continents = ["europe", "asia", "americas", "africa"];
 
-var legend = g.append('g')
-	.attr('transform', 'translate(' + (width-10) + ',' + (height-125) + ')')
+var legend = g
+	.append("g")
+	.attr("transform", "translate(" + (width - 10) + "," + (height - 125) + ")");
 
-continents.forEach(function(continent, i){
-	var legendRow = legend.append('g')
-		.attr('transform', 'translate(0, ' + (i*20) + ')')
+continents.forEach(function(continent, i) {
+	var legendRow = legend
+		.append("g")
+		.attr("transform", "translate(0, " + i * 20 + ")");
 
 	// Add legend color
-	legendRow.append('rect')
-		.attr('width', 10)
-		.attr('height', 10)
-		.attr('fill', color(continent))
-	
+	legendRow
+		.append("rect")
+		.attr("width", 10)
+		.attr("height", 10)
+		.attr("fill", color(continent));
+
 	// Add legend text
-	legendRow.append('text')
-		.attr('x', -10)
-		.attr('y', 10)
-		.attr('text-anchor', 'end')
-		.style('text-transform', 'capitalize')
-		.text(continent)
-})
+	legendRow
+		.append("text")
+		.attr("x", -10)
+		.attr("y", 10)
+		.attr("text-anchor", "end")
+		.style("text-transform", "capitalize")
+		.text(continent);
+});
 
 d3.json("data/data.json").then(function(data) {
 	// console.log(data);
@@ -135,16 +152,16 @@ d3.json("data/data.json").then(function(data) {
 
 	// Add in interval loop
 	// Add an update
-	d3.interval(function(){
-		year = (year < 214) ? year+1 : 0
-		update(cleanedData[year])
-	}, 100)
+	d3.interval(function() {
+		year = year < 214 ? year + 1 : 0;
+		update(cleanedData[year]);
+	}, 100);
 	// update(cleanedData[0]);
 });
 
 function update(data) {
 	// Transition variable
-	var t = d3.transition().duration(50)
+	var t = d3.transition().duration(50);
 
 	// JOIN new data with old elements
 	var circles = g.selectAll("circle").data(data, function(d) {
@@ -152,16 +169,21 @@ function update(data) {
 	});
 
 	// EXIT old elementsnot present in new data
-	circles.exit().attr('class', 'exit').remove()
+	circles
+		.exit()
+		.attr("class", "exit")
+		.remove();
 
 	// ENTER and UPDATE
 	circles
 		.enter()
 		.append("circle")
 		.attr("class", "enter")
-		.attr('fill', function(d){
-			return color(d.continent)
+		.attr("fill", function(d) {
+			return color(d.continent);
 		})
+		.on("mouseover", tip.show)
+		.on("mouseout", tip.hide)
 		// .attr("cy", function(d) {
 		// 	return y(d.life_exp);
 		// })
@@ -171,19 +193,19 @@ function update(data) {
 		// .attr("r", 5)
 		// UPDATE
 		.merge(circles)
-			.transition(t)
-			.attr("class", "enter")
-			.attr("cy", function(d) {
-				return y(d.life_exp);
-			})
-			.attr("cx", function(d) {
-				return x(d.income);
-			})
-			.attr("r", function(d){
-				return Math.sqrt(pop_area(d.population) / Math.PI)
-				// return pop(d.population) / Math.PI
-			});
-	
+		.transition(t)
+		.attr("class", "enter")
+		.attr("cy", function(d) {
+			return y(d.life_exp);
+		})
+		.attr("cx", function(d) {
+			return x(d.income);
+		})
+		.attr("r", function(d) {
+			return Math.sqrt(pop_area(d.population) / Math.PI);
+			// return pop(d.population) / Math.PI
+		});
+
 	// Update year label
-	yearLabel.text(+(year+ 1800))
+	yearLabel.text(+(year + 1800));
 }
